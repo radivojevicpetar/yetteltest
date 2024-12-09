ee7ab1ffed671c00750d25150d95df6950a182a7
radivojevicpetar <mrziggy49@gmail.com>
2024-12-09 06:14:00 +0100

    Update taskRoutes.ts

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,20 +1,12 @@
-// In taskRoutes.ts
+
 import express from 'express';
 import { createTask, getAllTasks, updateTask, deleteTask } from '../controllers/taskController';
-import { promiseDb } from '../server';  // Import the promiseDb from server
-
+import { promiseDb } from '../server';  
 const router = express.Router();
 
-// Create Task
 router.post('/', (req, res) => createTask(req, res, promiseDb));
-
-// Get All Tasks
 router.get('/', (req, res) => getAllTasks(req, res, promiseDb));
-
-// Update Task
 router.put('/:id', (req, res) => updateTask(req, res, promiseDb));
-
-// Delete Task
 router.delete('/:id', (req, res) => deleteTask(req, res, promiseDb));
 
 export default router;
